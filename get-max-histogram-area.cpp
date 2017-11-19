0c108993591322aa0a9f626462ddea50952ecaac
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-11-19 12:11:23 +0530

    Add files via upload

---
+++ get-max-histogram-area.cpp
@@ -0,0 +1,35 @@
+
+//aman kumar jha
+#include<bits/stdc++.h>
+using namespace std;
+int getmaxarea(int a[],int n)
+{
+    stack<int> s;
+    int i=0,maxarea=0,area;
+    while(i<n)
+    {
+        if(s.empty()||a[s.top()]<=a[i])
+            s.push(i++);
+        else
+        {
+            int top=s.top();
+            s.pop();
+            area=a[top]*(s.empty()?i:i-s.top()-1);
+            maxarea=max(maxarea,area);
+        }
+    }
+    while (!s.empty())
+    {
+        int top=s.top();
+        s.pop();
+        area=a[top]*(s.empty()?i:i-s.top()-1);
+        maxarea=max(area,maxarea);
+    }
+
+    return maxarea;
+}
+int main()
+{
+    int hist[]={6, 2, 5, 4, 5, 1, 6};;
+    printf("Area: %d\n",getmaxarea(hist,7));
+}
