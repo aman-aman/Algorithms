04973127e91bb6b7f8ddecc49678d33d0dc8e05b
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-11-21 08:57:29 +0530

    Add files via upload

---
+++ Cutting-Rod.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+//aman kumar jha
+int maxprofit(int a[],int n)
+{
+    int dp[n+1];
+    dp[0]=0;
+    for(int i=1;i<=n;i++)
+    {
+        int val=INT_MIN;
+        for(int j=0;j<i;j++)
+        {
+            val=max(val,a[j]+dp[i-j-1]);
+        }
+        dp[i]=val;
+    }
+
+    return dp[n];
+}
+int main()
+{
+    // length of rod is 8
+    int a[] = {1,5,8,9,10,17,17,20};
+    int n=8;
+    printf("Max Value: %d\n",maxprofit(a,n));
+}
