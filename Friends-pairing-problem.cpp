378ebb58d8485d5ac1a620ca61f685772c157a84
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-12-14 19:40:46 +0530

    Add files via upload

---
+++ Friends-pairing-problem.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+//aman kumar jha
+ll mod=1000000007;
+int main()
+{
+    ll t;
+    ll dp[101];
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=2;
+    for(ll i=3;i<=100;i++)
+        dp[i]=(dp[i-1]+((i-1)*dp[i-2]))%mod;
+
+
+        ll n;
+        scanf("%lld",&n);
+        printf("%lld\n",dp[n]);
+}
